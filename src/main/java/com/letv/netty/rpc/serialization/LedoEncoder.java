0d4b735eeb7bcf6dc342060db3aba4b72b92b655
jdyuhailang <yuhailang20140103@126.com>
2016-12-26 11:01:13 +0800

    初始化netty rpc

--- src/main/java/com/letv/netty/rpc/serialization/LedoEncoder.java
+++ src/main/java/com/letv/netty/rpc/serialization/LedoEncoder.java
@@ -13,8 +13,6 @@ import org.slf4j.LoggerFactory;
  * <p/>
  * Company: <a href=www.le.com>LeEco</a><br/>
  *
- * @author: <a href=mailto:wutao7@le.com>吴涛</a>
- * <br/>
  * @Date: 2016/08/23 15:02
  */
 public class LedoEncoder extends MessageToByteEncoder {
